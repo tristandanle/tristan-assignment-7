4c59c8f4c9d8aa5ef54d7cbded45fecd9f2f2305
tristandanle <letristandan@gmail.com>
2022-04-25 09:48:05 -0400

    Update CustomArrayListTest.java

--- src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
+++ src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
@@ -1,20 +1,13 @@
 package com.coderscampus.assignment7unittesting;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class CustomArrayListTest {
 
 	@Test
 	void should_add_new_item_to_list() {
-		// Three A's
-		// Arrange, Act, Assert
-
-		// Arrange
+		
 		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
 		sut.add(10);
 		sut.add(11);
 		sut.add(12);
@@ -27,58 +20,48 @@ class CustomArrayListTest {
 		sut.add(19);
 		sut.add(20);
 		
-		
-		
 		Integer expectedResult = sut.get(0);
-		//Integer expectedSize = sut.getSize();
-
-		// Assert
 
 		// assertEquals(10, sut.get(0));
 		assertEquals(10, expectedResult);
 		assertEquals(20, sut.get(10));
 		assertEquals(11, sut.getSize());
-
 	}
 
 	
 	@Test
 	void should_remove_item_to_list() {
 
-		// Arrange
 		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
-
 		sut.add(10);
 		sut.add(11);
 		sut.add(12);
 		sut.add(13);
+		sut.add(10);
+		sut.add(11);
+		sut.add(12);
+		sut.add(13);
+		sut.add(10);
+		sut.add(11);
+		sut.add(12);
+		sut.add(6,66);
+		
 		Integer expectedResult1 = sut.remove(3);
-
-		// Assert
+		
 		assertEquals(13, expectedResult1);
-		assertEquals(3, sut.getSize());
-
+		assertEquals(11, sut.getSize());
 	}
 
 	
 	@Test
 	void should_add_item_at_index_to_list() {
-
-		// Arrange
+		
 		CustomList<Integer> sut = new CustomArrayList<>();
 
-		// Act
 		sut.add(4,56);
 		sut.add(6,66);
 		
-		
-		//Integer expectedSize = sut.getSize();
-		// Assert
 		assertEquals(56, sut.get(4));
 		assertEquals(2, sut.getSize());
-
 	}
-
 }
