1ba7726b9f67f2cc6f32ffdae31611a06a0092d8
tristandanle <letristandan@gmail.com>
2022-05-08 17:54:27 -0400

    Update CustomArrayListTest.java

--- src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
+++ src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
@@ -10,7 +10,6 @@ class CustomArrayListTest {
 
 	@Test
 	void should_add_new_item_to_list() {
-		List<Integer> list = new ArrayList<>();
 		CustomList<Integer> sut = new CustomArrayList<>();
 		for ( int i = 0; i < 10 ; i ++   ) {
 			sut.add(i);
@@ -29,21 +28,25 @@ class CustomArrayListTest {
 	
 	@Test
 	void should_remove_item_to_list() {
-
+		
 		CustomList<Integer> sut = new CustomArrayList<>();
 		for ( int i = 0; i < 10 ; i ++   ) {
 			sut.add(i);
 		}
 		
-		sut.add(6,66);
-		sut.add(7,77);
+		sut.remove(9);
+		sut.remove(5);
+		sut.add(9,1000);
+		sut.add(2,2000);
+		sut.add(3,3000);
+		//sut.remove(45);
+		Integer expectedResult = sut.get(0);
+		
+		assertEquals(0, expectedResult);
+                assertEquals(2000, sut.get(2));
+       	        assertEquals(11, sut.getSize());
 		// Index out of bound
-		// sut.add(34,87);
-		Integer expectedResult1 = sut.remove(3);
-		
-		assertEquals(3, expectedResult1);
-		assertEquals(11, sut.getSize());
-		
+		//sut.add(34,87);
 	}
 
 	
@@ -60,6 +63,7 @@ class CustomArrayListTest {
 		sut.add(6,66);
 		
 		assertEquals(56, sut.get(4));
-		assertEquals(2, sut.getSize());
+		assertEquals(12, sut.getSize());
+		// sut.add(86,66);
 	}
 }
