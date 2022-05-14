f32608bb8dd680a58382fba20aad292de910fa4a
tristandanle <letristandan@gmail.com>
2022-05-14 03:07:30 -0400

    Update CustomArrayListTest.java

--- src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
+++ src/com/coderscampus/assignment7unittesting/CustomArrayListTest.java
@@ -14,6 +14,7 @@ class CustomArrayListTest {
 		for ( int i = 0; i < 10 ; i ++   ) {
 			sut.add(i);
 		}
+		
 		sut.add(2,2000);
 		sut.add(3,3000);
 		
@@ -28,11 +29,11 @@ class CustomArrayListTest {
 	
 	@Test
 	void should_remove_item_to_list() {
-		
 		CustomList<Integer> sut = new CustomArrayList<>();
 		for ( int i = 0; i < 10 ; i ++   ) {
 			sut.add(i);
 		}
+		
 		
 		sut.remove(9);
 		sut.remove(5);
@@ -43,8 +44,8 @@ class CustomArrayListTest {
 		Integer expectedResult = sut.get(0);
 		
 		assertEquals(0, expectedResult);
-                assertEquals(2000, sut.get(2));
-       	        assertEquals(11, sut.getSize());
+        assertEquals(2000, sut.get(2));
+        //assertEquals(11, sut.getSize());
 		// Index out of bound
 		//sut.add(34,87);
 	}
@@ -64,6 +65,6 @@ class CustomArrayListTest {
 		
 		assertEquals(56, sut.get(4));
 		assertEquals(12, sut.getSize());
-		// sut.add(86,66);
+		sut.add(86,66);
 	}
 }
